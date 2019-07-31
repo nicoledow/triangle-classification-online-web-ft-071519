aa43ca3f854e2aaf0f9bd1020de93cc9dadc6889
Nicole Dow <nikki.dow71@gmail.com>
2019-07-31 16:54:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,11 @@
 class Triangle
-  attr_accessor 
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
   
   
 end
